8ef9e16c52017398b541614984ec64e235882cec
K1NGD4VID <adesayafuhad5@gmail.com>
2026-04-22 11:27:55 +0100

    Implement command-line argument handling and file reading in main function

--- go-reloaded-logic/text_formatter_cli/text_formatter_cli.go
+++ go-reloaded-logic/text_formatter_cli/text_formatter_cli.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"regexp"
+)
+
 func main() {
+
+	if len(os.Args) < 2 {
+		fmt.Println("Usage := go run . <filename>.txt")
+		return
+	}
+	filename := os.Args[1]
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Printf("Error reading file %s\n", err)
+		return
+	}
+	s := string(data)
+	revTag := regexp.MustCompile(`\((reverse|double|shout)\)`)
+	revTagmatch := revTag.FindStringSubmatchIndex(s)
+	fmt.Println(revTagmatch)
 
 }
